2fc940d70cd91643424616a4e9f4e3ab7bce3671
redxtreme <zahopper@mtu.edu>
2018-02-17 12:57:55 -0600

    Browser control started

--- web-control.py
+++ web-control.py
@@ -5,3 +5,11 @@ browser = webdriver.Safari()
 url = 'https://my.forksmealplanner.com/#!/archive'
 # print(creds.login['p'])
 browser.get(url)
+
+login_email_id = 'login-email'
+login_pass_id = 'login-password'
+
+login_email_elem = browser.find_element_by_id(login_email_id)
+login_pass_elem = browser.find_element_by_id(login_pass_id)
+
+login_email_elem.click()
